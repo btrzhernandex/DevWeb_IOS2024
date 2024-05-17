c503672f3c576b7096aca9f40ac0fe8f89aa8910
btrzhernandex <164105253+btrzhernandex@users.noreply.github.com>
2024-05-17 23:54:40 +0000

    aulas do ios

--- AULAS DE JS/01 INTRODUÇÃO AO JS/main.js
+++
@@ -1,1 +0,0 @@
-alert('vamos aprender js');
---
+++ AULAS DE JS/02 CONSOLE, VARIÁVEIS E OPERADORES/atividade.js
@@ -0,0 +1,5 @@
+const nome = ('Beatriz');
+const altura = (1,69);
+const peso = (59);
+const imc = peso / (altura * peso);
+console.log('Meu nome é ${Beatriz}, possuo ${1,69} metros e peso ${59} kg, meu imc atual é de ${imc.toFixed(2)}.')
