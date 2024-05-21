312040447cd906b8970ebbd6c964b3a53f06707c
btrzhernandex <164105253+btrzhernandex@users.noreply.github.com>
2024-05-21 23:58:17 +0000

    aulas do ios

---
+++ AULAS DE JS/01 INTRODUÇÃO/main.js
@@ -0,0 +1,16 @@
+window.alert('Olá!');
+alert('Seja bem vindo');
+document.title = "nova pagina criada";
+let section = document.createElement('h1');
+section.innerHTML = "Manipulando o DOM";
+document.body.appendChild(section);
+section.style.fontFamily = 'arial black'
+
+let article = document.createElement('p');
+article.innerHTML = "instituto da oportunidade social";
+document.body.appendChild(article);
+article.style.borderBottom = 'solid 3px #000';
+article.style.backgroundColor = '#873'
+
+//arrumar em casa. Manipulando DOM PT1 ATIVIDADE.
+
